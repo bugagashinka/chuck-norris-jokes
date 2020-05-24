853eb7f5a65ed816f9bbd6881ccac9501faa97be
bugagashinka <voloshyn.andrew@gmail.com>
2020-05-24 23:37:22 +0300

    Updates

---
+++ precache-manifest.391e2ef913eace33be321c86cf0d2f07.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f6d6d8bc3e0ee726be59f3dc21d80a9",
+    "url": "/chuck-norris-jokes/index.html"
+  },
+  {
+    "revision": "49276f2a1c85ef6082ca",
+    "url": "/chuck-norris-jokes/static/css/main.215a1a11.chunk.css"
+  },
+  {
+    "revision": "ebff7b209cbdb2248abd",
+    "url": "/chuck-norris-jokes/static/js/2.2ae088d8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/chuck-norris-jokes/static/js/2.2ae088d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49276f2a1c85ef6082ca",
+    "url": "/chuck-norris-jokes/static/js/main.717be193.chunk.js"
+  },
+  {
+    "revision": "e83e247c17babef87599",
+    "url": "/chuck-norris-jokes/static/js/runtime-main.1b027245.js"
+  },
+  {
+    "revision": "cd24cee9e69c35240033aa01666f864a",
+    "url": "/chuck-norris-jokes/static/media/error-icon.cd24cee9.svg"
+  },
+  {
+    "revision": "3369cbd8b994c773639f1296b6af391b",
+    "url": "/chuck-norris-jokes/static/media/heart-inactive.3369cbd8.svg"
+  },
+  {
+    "revision": "2cedafc951372ad8f7e11384cc7780ce",
+    "url": "/chuck-norris-jokes/static/media/heart.2cedafc9.svg"
+  },
+  {
+    "revision": "0f10cee4fb9784272c90da4cd403996d",
+    "url": "/chuck-norris-jokes/static/media/link-icon.0f10cee4.svg"
+  },
+  {
+    "revision": "889f54a4e41027303666188c35a7977c",
+    "url": "/chuck-norris-jokes/static/media/menu-close-icon.889f54a4.svg"
+  },
+  {
+    "revision": "ffa892cbe43b2f73c3e38071a387401c",
+    "url": "/chuck-norris-jokes/static/media/menu-icon.ffa892cb.svg"
+  },
+  {
+    "revision": "11fa47de8375e84a4ed05dadfde82103",
+    "url": "/chuck-norris-jokes/static/media/roboto-bold-webfont.11fa47de.woff2"
+  },
+  {
+    "revision": "1c6a45a7f6c615eb35e5ff519302927a",
+    "url": "/chuck-norris-jokes/static/media/roboto-bold-webfont.1c6a45a7.woff"
+  },
+  {
+    "revision": "06bf4ee8c54a97b391cfc5f14bf56c64",
+    "url": "/chuck-norris-jokes/static/media/roboto-medium-webfont.06bf4ee8.woff"
+  },
+  {
+    "revision": "a9259f5e211b2be10bfad5dff1501923",
+    "url": "/chuck-norris-jokes/static/media/roboto-medium-webfont.a9259f5e.woff2"
+  },
+  {
+    "revision": "2e7163b05a30ac822cc1f670feeb4dd0",
+    "url": "/chuck-norris-jokes/static/media/roboto-regular-webfont.2e7163b0.woff2"
+  },
+  {
+    "revision": "f68f2045befe4e88ada19fca5839745c",
+    "url": "/chuck-norris-jokes/static/media/roboto-regular-webfont.f68f2045.woff"
+  },
+  {
+    "revision": "71773f6e553cd414b6486cdde5abdb88",
+    "url": "/chuck-norris-jokes/static/media/text-icon.71773f6e.svg"
+  }
+]);
